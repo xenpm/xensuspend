779199689d552e31b3c29feb951015730213dbb3
Van Do <van.do.xw@renesas.com>
2021-03-24 18:47:33 +0700

    Use lower version of pyxs
    Signed-off-by: Van Do <van.do.xw@renesas.com>

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(name='xensuspend',
       version='0.0.1',
       packages=['xensuspend'],
       description='Xen system suspend coordinator',
-      url='https://github.com/xen-troops/xensuspend',
+      url='https://github.com/xenpm/xensuspend',
       license='GPLv2',
-      install_requires=["pyxs>=0.3"],
+      install_requires=["pyxs=0.3"],
       scripts=['bin/xensuspend']
     )
